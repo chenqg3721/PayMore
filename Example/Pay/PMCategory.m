7e27bed49ce092aa8d5d9677afeae15cee912126
chenqg666 <chenqg666@gmail.com>
2017-12-26 17:55:25 +0800

    add .

---
+++ Example/Pay/PMCategory.m
@@ -0,0 +1,13 @@
+//
+//  PMCategory.m
+//  Pay
+//
+//  Created by chenqg on 2017/12/26.
+//  Copyright © 2017年 chenqg666@gmail.com. All rights reserved.
+//
+
+#import "PMCategory.h"
+
+@implementation PMCategory
+
+@end
